c9a5ab3de26864775232f9d0256bb3f032b233ef
Andrew Davisson <akdavisson4@gmail.com>
2019-06-21 21:35:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,3 +4,4 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 2, 1)
 INSERT INTO subgenres (name) VALUES ("wizardy");
 INSERT INTO subgenres (name) VALUES ("time-travel");
 
+INSERT INTO authors (name) VALUES ("")
